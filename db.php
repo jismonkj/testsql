7c5f86b5f4e98b98ee43736b534e490092ad573e
jismonkj <33921319+jismonkj@users.noreply.github.com>
2018-03-06 12:01:36 +0530

    db connection to sql instance

--- db.php
+++ db.php
@@ -6,6 +6,7 @@
 
 // $conn = mysqli_connect($server, $user, $password, $db) or die('error');
 
-$conn = mysqli_connect('localhost', 'root', '', 'lab') or die('error');
+//$conn = mysqli_connect('localhost', 'root', '', 'lab') or die('error');
+$con=new mysqli (NULL, ”root”, ”123”, ”lab”, NULL, ”/cloudsql/cloudsqltest-197006:us-central1:sqltest“);
 
 ?>
